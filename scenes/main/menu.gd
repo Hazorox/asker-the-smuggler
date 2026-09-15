796e7a855bf611c43102ffadeca8335453ba4f56
Hamza Awad <hamzaawad8899@gmail.com>
2026-09-15 18:25:26 +0300

    Add main menu buttons navigation with keys

--- scenes/main/menu.gd
+++ scenes/main/menu.gd
@@ -1,12 +1,17 @@
 extends Control
 
-@onready var buttons := [$start,$option,$exit]
+@onready var buttons :Array[Button]= [$start,$option,$exit]
+var focused := 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	buttons[focused].grab_focus()
+	if Input.is_action_just_pressed("down"):
+		if focused==2:
+			focused=0
+		else:
+			focused+=1
+	elif Input.is_action_just_pressed("up"):
+		if focused==0:
+			focused=2
+		else:
+			focused-=1

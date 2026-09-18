387788634eba0b3070f66d966d955e058d4f6c5c
vii-abdullatif <dows5750@gmail.com>
2026-09-18 19:02:31 +0300

    Added catch mechanism and a game over screen

--- scenes/askar/askar.gd
+++ scenes/askar/askar.gd
@@ -1,8 +1,11 @@
+class_name Asker
 extends CharacterBody2D
 
 const SPEED := 250
 
 @onready var asker_animated_sprite: AnimatedSprite2D = $AskerAnimatedSprite
+
+var is_player_caught:= false
 
 func _process(_delta: float) -> void:
 	var direction := Input.get_vector("left", "right", "up", "down")
@@ -21,3 +24,7 @@ func set_animation() -> void:
 		asker_animated_sprite.play("walking")
 	else:
 		asker_animated_sprite.play("idle")
+
+func _on_cuaght_detection_area_area_entered(area: Area2D) -> void:
+	if area.is_in_group("security"):
+		CaughtDetection.is_player_caught = true
--- scenes/school_parts/corridor.gd
+++ scenes/school_parts/corridor.gd
@@ -1,12 +1,20 @@
 extends Node2D
 
 @onready var pause_menu_scene := preload("res://scenes/ui/pause_menu.tscn")
+@onready var game_oveer_scene := preload("res://scenes/ui/game_over_screen.tscn")
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("pause"):
-		pause_game()
+	pause_game()
+	game_over()
 
 func pause_game() -> void:
-	get_tree().paused = true
-	var pause_menu = pause_menu_scene.instantiate()
-	add_child(pause_menu)
+	if Input.is_action_just_pressed("pause"):
+		get_tree().paused = true
+		var pause_menu = pause_menu_scene.instantiate()
+		add_child(pause_menu)
+
+func game_over() -> void:
+	if CaughtDetection.is_player_caught == true:
+		get_tree().paused = true
+		var gameover_menu = game_oveer_scene.instantiate()
+		add_child(gameover_menu)

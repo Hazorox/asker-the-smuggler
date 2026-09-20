761ab582c090735be39f8e7b0ef90c0eec554829
Hamza Awad <hamzaawad8899@gmail.com>
2026-09-21 02:37:49 +0300

    Add 3 modes and options key

--- scenes/main/menu.gd
+++ scenes/main/menu.gd
@@ -1,5 +1,5 @@
 extends Control
-@onready var buttons :Array[Button]= [$start,$hunt,$option]
+@onready var buttons :Array[Button]= [$start,$hunt,$hungry,$option]
 var focused := 0
 
 func _process(delta: float) -> void:
@@ -8,12 +8,16 @@ func _process(delta: float) -> void:
 	
 	# Change focused index based on click and current focused index
 	if Input.is_action_just_pressed("down"):
-		if focused==2:
+		if focused==3:
 			focused=0
 		else:
 			focused+=1
 	elif Input.is_action_just_pressed("up"):
 		if focused==0:
-			focused=2
+			focused=3
 		else:
 			focused-=1
+
+
+func _on_option_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/options/options.tscn")

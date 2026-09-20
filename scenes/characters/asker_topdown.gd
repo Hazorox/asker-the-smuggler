3d8486aa7872ffea60bfaf97c56045beab6c1fcd
Hamza Awad <hamzaawad8899@gmail.com>
2026-09-20 21:28:32 +0300

    Add base character for the player

---
+++ scenes/characters/asker_topdown.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+
+@export var SPEED = 300.0
+@export var sprintAmount = 150
+@onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(_delta: float) -> void:
+	var direction_y := Input.get_axis("down","up")
+	var direction := Input.get_axis("left", "right")
+	
+	if direction_y:
+		velocity.y=direction_y*SPEED
+	else:
+		velocity.y = move_toward(velocity.y,0,SPEED)
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+
+
+	move_and_slide()

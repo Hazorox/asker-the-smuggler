d241f596f9c84e4888a097e0addb3b8810948b86
Hamza Awad <hamzaawad8899@gmail.com>
2026-09-20 21:52:21 +0300

    Add asker topdown character

--- scenes/characters/asker_topdown.gd
+++ scenes/characters/asker_topdown.gd
@@ -6,7 +6,7 @@ extends CharacterBody2D
 @onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(_delta: float) -> void:
-	var direction_y := Input.get_axis("down","up")
+	var direction_y := Input.get_axis("up","down")
 	var direction := Input.get_axis("left", "right")
 	
 	if direction_y:
@@ -17,7 +17,14 @@ func _physics_process(_delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-
+	if velocity.y>0:
+		sprite.play("down")
+	elif velocity.y <0:
+		sprite.play("up")
+	else:
+		if velocity.x!=0:
+			sprite.play("right")
+		else:
+			sprite.play("idle")
 
 	move_and_slide()

9dc46fb6044e91865aba08c5e63a063c19b02726
Hamza Awad <hamzaawad8899@gmail.com>
2026-09-20 21:53:00 +0300

    Fix bug in 'Return to Main Menu' Option

--- scenes/ui/game_over_screen.gd
+++ scenes/ui/game_over_screen.gd
@@ -16,3 +16,4 @@ func _on_restart_pressed() -> void:
 func _on_main_menu_pressed() -> void:
 	print("main menu pressed")
 	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
